9a8123c8773e23ed47e3e85f1d329b69c222b735
PowFox <lapienis.paulius@yahoo.com>
2024-11-06 21:47:02 +0200

    Implemented a basic Achievement structure

--- employee.py
+++ employee.py
@@ -1,8 +1,11 @@
+class Achievement: ...
+
 class Employee:
-    def __init__(self, vardas: str, pavarde: str, salary: int):
+    def __init__(self, vardas: str, pavarde: str, salary: int, achievements: list[Achievement]):
         self.vardas = vardas
         self.pavarde = pavarde
         self.salary = salary
+        self.achievements = achievements
 
     def calc_post_tax(self):
         return self.salary * 0.61
