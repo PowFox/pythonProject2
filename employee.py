d03e5f66949fb6adec9d0cb73c2a4ec3cd4ca624
PowFox <lapienis.paulius@yahoo.com>
2024-11-06 20:04:48 +0200

    Implemented salary saving in Employee class

--- employee.py
+++ employee.py
@@ -1,4 +1,8 @@
 class Employee:
-    def __init__(self, vardas, pavarde):
+    def __init__(self, vardas, pavarde, salary):
         self.vardas = vardas
         self.pavarde = pavarde
+        self.salary = salary
+
+    def __repr__(self):
+        return f"| {self.vardas} {self.pavarde} - {self.salary} |"
